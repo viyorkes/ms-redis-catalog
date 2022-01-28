5262c30d5876a53442515fa27949510d370a5921
victor <victorsleiman25@gmail.com>
2022-01-28 13:19:28 -0300

    Creating Catalog Redis Project

---
+++ src/main/java/com/redis/catalog/RedisCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.redis.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redis/catalog/RedisCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
