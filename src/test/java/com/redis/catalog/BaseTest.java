709b244c02ce68dc2a478893ad42b492e494940f
victor <victorsleiman25@gmail.com>
2022-01-28 22:21:57 -0300

    Creating a class BaseTest with some redis features

---
+++ src/test/java/com/redis/catalog/BaseTest.java
@@ -0,0 +1,28 @@
+package com.redis.catalog;
+
+import com.redis.catalog.config.RedissonConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.redisson.api.RedissonReactiveClient;
+
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public abstract class BaseTest {
+
+
+    private final RedissonConfig redissonConfig = new RedissonConfig();
+    protected RedissonReactiveClient client;
+
+    @BeforeAll
+    public void  setClient(){
+        this.client = this.redissonConfig.getClient().reactive();
+
+    }
+
+    @AfterAll
+    public void shutdown(){
+        this.client.shutdown();
+    }
+}
